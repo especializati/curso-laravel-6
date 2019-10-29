fcf666f485d480031cee89de68a4eb6daa8a5b2d
Carlos Ferreira <conta@especializati.com.br>
2019-10-29 08:24:47 -0300

    Laravel Controllers

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+
+        // $this->middleware('auth');
+        /*$this->middleware('auth')->only([
+            'create', 'store'
+        ]);*/
+        $this->middleware('auth')->except([
+            'index', 'show'
+        ]);
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'Listagem dos produtos';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "Detalhes do produto {$id}";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+Route::resource('products', 'ProductController'); // ->middleware('auth');
+/*
+Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::put('products/{id}', 'ProductController@update')->name('products.update');
+Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('products/create', 'ProductController@create')->name('products.create');
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::post('products', 'ProductController@store')->name('products.store');
+*/
+
+
 Route::get('/login', function () {
     return 'Login';
 })->name('login');
