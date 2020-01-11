0f8d93f99d4a5950a48624eb8ab86754889085f1
Carlos Ferreira <carlos.ferreira@inevent.com>
2020-01-11 10:29:12 -0300

    Create Factory Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->word,
+        'description' => $faker->sentence(),
+        'price' => 12.2,
+    ];
+});
---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Product::class, 100)->create();
+    }
+}
